3314c260b7c76b0798bad85df22869ce796a8328
Martin Tamm <mtammm@gmail.com>
2019-11-06 22:32:38 +0200

    Add project files.

---
+++ HexToDec/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace HexToDec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.copyToClipboardButton = new System.Windows.Forms.Button();
+            this.clearAllButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.inputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(681, 98);
+            this.inputTextBox.TabIndex = 0;
+            this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.calculateButton.Location = new System.Drawing.Point(12, 116);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(681, 49);
+            this.calculateButton.TabIndex = 1;
+            this.calculateButton.Text = "Convert To Decimal";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.outputTextBox.Location = new System.Drawing.Point(12, 171);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(681, 98);
+            this.outputTextBox.TabIndex = 2;
+            this.outputTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // copyToClipboardButton
+            // 
+            this.copyToClipboardButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.copyToClipboardButton.Location = new System.Drawing.Point(328, 275);
+            this.copyToClipboardButton.Name = "copyToClipboardButton";
+            this.copyToClipboardButton.Size = new System.Drawing.Size(365, 51);
+            this.copyToClipboardButton.TabIndex = 3;
+            this.copyToClipboardButton.Text = "Copy To Clipboard";
+            this.copyToClipboardButton.UseVisualStyleBackColor = true;
+            this.copyToClipboardButton.Click += new System.EventHandler(this.copyToClipboardButton_Click);
+            // 
+            // clearAllButton
+            // 
+            this.clearAllButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.clearAllButton.Location = new System.Drawing.Point(12, 275);
+            this.clearAllButton.Name = "clearAllButton";
+            this.clearAllButton.Size = new System.Drawing.Size(310, 51);
+            this.clearAllButton.TabIndex = 4;
+            this.clearAllButton.Text = "Clear All";
+            this.clearAllButton.UseVisualStyleBackColor = true;
+            this.clearAllButton.Click += new System.EventHandler(this.clearAllButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 339);
+            this.Controls.Add(this.clearAllButton);
+            this.Controls.Add(this.copyToClipboardButton);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.inputTextBox);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Hex To Decimal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Button copyToClipboardButton;
+        private System.Windows.Forms.Button clearAllButton;
+    }
+}
+
---
+++ HexToDec/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculationItems;
+
+namespace HexToDec
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        HexToDecimal converter = new HexToDecimal();
+
+        public string[] populateInputArray()
+        {
+            string rawInput = inputTextBox.Text;
+            // check for hex elements only
+            
+            string[] splitInput = rawInput.Split(' ');
+            return splitInput;
+        }
+
+        public bool HexInStringCStyle(string test)
+        {
+            // For C-style hex notation (0xFF) you can use @"\A\b(0[xX])?[0-9a-fA-F]+\b\Z"
+            return System.Text.RegularExpressions.Regex.IsMatch(test, @"\A\b(0[xX])?[0-9a-fA-F]+\b\Z");
+        }
+
+        public bool HexInStringNormal(string test)
+        {
+            // For C-style hex notation (0xFF) you can use @"\A\b(0[xX])?[0-9a-fA-F]+\b\Z"
+            return System.Text.RegularExpressions.Regex.IsMatch(test, @"\A\b[0 - 9a - fA - F] +\b\Z");
+        }
+
+        public void convertToDecimal(string[] input)
+        {
+            string output = "";
+            foreach(var item in input)
+            {
+                //check for clean hex inputs
+                if (HexInStringCStyle(item) || HexInStringNormal(item))
+                {
+                    int answer = Convert.ToInt32(item, 16);
+                    outputTextBox.AppendText(answer + " ");
+                }
+                else
+                {
+                    outputTextBox.AppendText($" >>>Bad Format>>>{ item }<<<");
+                }
+                //Convert.ToInt64(hexValue, 16);
+                
+            }
+            
+        }
+
+        private void inputTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            
+            convertToDecimal(populateInputArray());
+        }
+
+        private void copyToClipboardButton_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(outputTextBox.Text);
+        }
+
+        private void clearAllButton_Click(object sender, EventArgs e)
+        {
+            inputTextBox.Clear();
+            outputTextBox.Clear();
+        }
+    }
+}
