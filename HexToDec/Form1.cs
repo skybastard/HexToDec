ab7b27f01abc723d2ee3bdc5951a023776c12455
Martin Tamm <mtammm@gmail.com>
2022-01-30 18:30:43 +0200

    test

--- HexToDec/Form1.cs
+++ HexToDec/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using CalculationItems;
-
+// Comment for merging test
 namespace HexToDec
 {
     public partial class Form1 : Form
